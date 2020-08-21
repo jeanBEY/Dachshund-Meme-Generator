4c22e35955f66c2b48cab4c40e24a470ace899d0
Jean <jeanvallybeato@gmail.com>
2020-08-20 17:02:53 -0700

    Created text form & generate meme button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 import MemeGenerator from "./MemeGenerator"
-import './App.css';
+import Header from "./Header"
 
 function App() {
   return(
     <div>
-      <p>Hello Dachshund</p>
+      <Header />
       <MemeGenerator />
     </div>
   )
--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -5,14 +5,72 @@ class MemeGenerator extends Component{
 	constructor(){
 		super()
 		this.state={
+
+			toptext:"", 
+			bottomtext:"",
+			randomImg:"https://images.dog.ceo/breeds/dachshund/dachshund-3.jpg",
+			object:{},
+			allmemeImgs:[]
 			
 		}
+
+		this.handleChange = this.handleChange.bind(this)
+		this.handleSubmit = this.handleSubmit.bind(this)
 	}
 
+	componentDidMount(){
+		fetch("https://dog.ceo/api/breed/dachshund/images")
+			.then(response => response.json())
+			.then(response => {
+				const memes = response.message
+				this.setState({ allmemeImgs: memes })
+			})
+	}
+
+	handleChange(event){
+		const {name, value} = event.target
+		this.setState({[name]: value})
+		console.log(this.toptext)
+		console.log(this.bottomtext)
+	}
+
+	handleSubmit(event){
+		event.preventDefault()
+		const randNum = Math.floor(Math.random() * this.state.allmemeImgs.length)
+		const randMemeImg = this.state.allmemeImgs[randNum]
+		this.setState({ randomImg: randMemeImg })
+	}
 
 	render(){
 		return(
 			<div>
+				<form onSubmit={this.handleSubmit} alt="">
+
+				<input 
+					type="text"
+					name="toptext"
+					placeholder="Top Text"
+					value={this.state.toptext}
+					onChange={this.handleChange}
+				/>
+
+				<input 
+					type="text"
+					name="bottomtext"
+					placeholder="Botom Text"
+					value={this.state.bottomtext}
+					onChange={this.handleChange}
+				/>
+
+				<button>Generate</button>
+
+				</form>
+
+				<div>
+					<img src={this.state.randomImg} alt=""/>
+					<h2>{this.state.toptext}</h2>
+					<h2>{this.state.bottomtext}</h2>
+				</div>
 			</div>
 		)
 	}
